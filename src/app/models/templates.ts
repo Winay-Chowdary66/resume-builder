517572ae40434c2961f9aeeba12168bea5fbdf9c
Winay-Chowdary66 <uec17107@rmd.ac.in>
2021-10-16 15:10:09 +0530

    Added Loading, Template Comp && 404-Page Not Found Features.

---
+++ src/app/models/templates.ts
@@ -0,0 +1,7 @@
+export interface Templates {
+  templatesList : Template[];
+}
+
+export interface Template {
+  id : Number
+}
--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Template, Templates } from '../models/templates';
+import { TemplateService } from './template.service';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.css'],
 })
-export class TemplateComponent implements OnInit {
-  constructor(private router: Router) {}
+export class TemplateComponent implements OnInit {  
+ 
+  templates:Template[] = [];
+  constructor(private router: Router, private templateService : TemplateService) {}
 
-  template1() {
-    this.router.navigateByUrl('/template-1');
-  };
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.templateService.getTemplates().subscribe( (res:Templates) =>{
+      // console.log(res.templatesList[0].imgSrc);
+      this.templates = res.templatesList;
+    });
+    
+  }
+
+  getTemplate(templateId:Number):void{
+    this.router.navigate(['edit/template', templateId])
+  }
 }
