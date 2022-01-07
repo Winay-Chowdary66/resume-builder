733b73c75f271648fb50fc17b488d89926a3eacc
Winay-Chowdary66 <uec17107@rmd.ac.in>
2022-01-07 15:53:41 +0530

    Updated skills summary

--- src/app/templates/edit-template/edit-template.component.ts
+++ src/app/templates/edit-template/edit-template.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit, DoCheck } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  DoCheck,
+  ViewChild,
+  QueryList,
+  ElementRef,
+  AfterViewInit,
+  ViewChildren,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { TemplateService } from 'src/app/template/template.service';
 import { ExperienceForm } from '../template-one/template-one.component';
@@ -7,8 +18,9 @@ import { ExperienceForm } from '../template-one/template-one.component';
   selector: 'app-edit-template',
   templateUrl: './edit-template.component.html',
   styleUrls: ['./edit-template.component.css'],
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EditTemplateComponent implements OnInit, DoCheck {
+export class EditTemplateComponent implements OnInit, DoCheck, AfterViewInit {
   resumeForm = new FormGroup({
     firstName: new FormControl('Vinay Chowdary', []),
     jobRole: new FormControl('Engineer', []),
@@ -58,7 +70,7 @@ export class EditTemplateComponent implements OnInit, DoCheck {
       address: new FormControl('AP, India- 522124', []),
       email: new FormControl('vinayannangi.123@gmail.com'),
       phone: new FormControl('+91 9492117926'),
-      site: new FormControl('github.com/winay-chowdary66')
+      site: new FormControl('github.com/winay-chowdary66'),
     }),
     skills: new FormGroup({
       skill1: new FormControl('Python', []),
@@ -72,16 +84,16 @@ export class EditTemplateComponent implements OnInit, DoCheck {
 
   public firstName = '';
   public jobRole = '';
-  public experienceForm: ExperienceForm = new CommonForm()
-  public projectsForm: ExperienceForm = new CommonForm()
+  public experienceForm: ExperienceForm = new CommonForm();
+  public projectsForm: ExperienceForm = new CommonForm();
 
-  public educationForm: ExperienceForm = new CommonForm()
+  public educationForm: ExperienceForm = new CommonForm();
   public contactForm = {
     address: 'AP, India- 522124',
     email: '',
     phone: '+91 9492117926',
     site: 'github.com/winay-chowdary66',
-  }
+  };
   public skillsForm = {
     skill1: 'Python',
     skill2: 'Java',
@@ -89,11 +101,11 @@ export class EditTemplateComponent implements OnInit, DoCheck {
     skill4: 'C',
     skill5: 'HTML',
     skill6: 'CSS',
-  }
+  };
 
   public editBtnProperty = '';
 
-  public isExpOrProjOrEdu = false
+  public isExpOrProjOrEdu = false;
 
   getFormControl(name: string) {
     return this.resumeForm?.get(name);
@@ -123,7 +135,29 @@ export class EditTemplateComponent implements OnInit, DoCheck {
     return this.experience?.get(name) as FormControl;
   }
 
-  constructor(private _tempService: TemplateService) {}
+  @ViewChildren('skillBtn1') skillButton1!: QueryList<ElementRef<HTMLElement>>;
+  @ViewChildren('skillBtn2')
+  skillButton2!: QueryList<ElementRef<HTMLElement>>;
+  @ViewChildren('skillBtn3')
+  skillButton3!: QueryList<ElementRef<HTMLElement>>;
+  @ViewChildren('skillBtn4')
+  skillButton4!: QueryList<ElementRef<HTMLElement>>;
+  @ViewChildren('skillBtn5')
+  skillButton5!: QueryList<ElementRef<HTMLElement>>;
+  @ViewChildren('skillBtn6')
+  skillButton6!: QueryList<ElementRef<HTMLElement>>;
+
+  public skill1Value: string = '3';
+  public skill2Value: string = '3';
+  public skill3Value: string = '2';
+  public skill4Value: string = '3';
+  public skill5Value: string = '3';
+  public skill6Value: string = '2';
+
+  constructor(
+    private _tempService: TemplateService,
+    private _changeDetector: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.firstName = this.resumeForm.get('firstName')?.value;
@@ -145,32 +179,69 @@ export class EditTemplateComponent implements OnInit, DoCheck {
     this.projectsForm.endingYear = this.projects.get('endingYear')?.value;
     this.projectsForm.point1 = this.projects.get('point1')?.value;
     this.projectsForm.point2 = this.projects.get('point2')?.value;
-    
-    this.educationForm.title = this.resumeForm.get('education')?.get('title')?.value;
-    this.educationForm.organization = this.resumeForm.get('education')?.get('organization')?.value;
-    this.educationForm.roleInProj = this.resumeForm.get('education')?.get('roleInProj')?.value;
-    this.educationForm.startingYear = this.resumeForm.get('education')?.get('startingYear')?.value;
-    this.educationForm.endingYear = this.resumeForm.get('education')?.get('endingYear')?.value;
-    this.educationForm.point1 = this.resumeForm.get('education')?.get('point1')?.value;
-    this.educationForm.point2 = this.resumeForm.get('education')?.get('point2')?.value;
-    
-    this.contactForm.address = this.resumeForm.get('contact')?.get('address')?.value;
-    this.contactForm.email = this.resumeForm.get('contact')?.get('email')?.value;
-    this.contactForm.phone = this.resumeForm.get('contact')?.get('phone')?.value;
+
+    this.educationForm.title = this.resumeForm
+      .get('education')
+      ?.get('title')?.value;
+    this.educationForm.organization = this.resumeForm
+      .get('education')
+      ?.get('organization')?.value;
+    this.educationForm.roleInProj = this.resumeForm
+      .get('education')
+      ?.get('roleInProj')?.value;
+    this.educationForm.startingYear = this.resumeForm
+      .get('education')
+      ?.get('startingYear')?.value;
+    this.educationForm.endingYear = this.resumeForm
+      .get('education')
+      ?.get('endingYear')?.value;
+    this.educationForm.point1 = this.resumeForm
+      .get('education')
+      ?.get('point1')?.value;
+    this.educationForm.point2 = this.resumeForm
+      .get('education')
+      ?.get('point2')?.value;
+
+    this.contactForm.address = this.resumeForm
+      .get('contact')
+      ?.get('address')?.value;
+    this.contactForm.email = this.resumeForm
+      .get('contact')
+      ?.get('email')?.value;
+    this.contactForm.phone = this.resumeForm
+      .get('contact')
+      ?.get('phone')?.value;
     this.contactForm.site = this.resumeForm.get('contact')?.get('site')?.value;
 
-    this.skillsForm.skill1 = this.resumeForm.get('skills')?.get('skill1')?.value;
-    this.skillsForm.skill2 = this.resumeForm.get('skills')?.get('skill2')?.value;
-    this.skillsForm.skill3 = this.resumeForm.get('skills')?.get('skill3')?.value;
-    this.skillsForm.skill4 = this.resumeForm.get('skills')?.get('skill4')?.value;
-    this.skillsForm.skill5 = this.resumeForm.get('skills')?.get('skill5')?.value;
-    this.skillsForm.skill6 = this.resumeForm.get('skills')?.get('skill6')?.value;
+    this.skillsForm.skill1 = this.resumeForm
+      .get('skills')
+      ?.get('skill1')?.value;
+    this.skillsForm.skill2 = this.resumeForm
+      .get('skills')
+      ?.get('skill2')?.value;
+    this.skillsForm.skill3 = this.resumeForm
+      .get('skills')
+      ?.get('skill3')?.value;
+    this.skillsForm.skill4 = this.resumeForm
+      .get('skills')
+      ?.get('skill4')?.value;
+    this.skillsForm.skill5 = this.resumeForm
+      .get('skills')
+      ?.get('skill5')?.value;
+    this.skillsForm.skill6 = this.resumeForm
+      .get('skills')
+      ?.get('skill6')?.value;
 
     this._tempService.getBtnProperty().subscribe((property) => {
       this.editBtnProperty = property;
     });
 
-
+    // const skillBtn = document.querySelectorAll('.inputrange-btn');
+    // skillBtn.forEach((btn) => {
+    //   btn.addEventListener('click', () => {
+    //     alert('Clicked');
+    //   });
+    // });
   }
 
   ngDoCheck(): void {
@@ -178,8 +249,12 @@ export class EditTemplateComponent implements OnInit, DoCheck {
     this._tempService.sendJobRole(this.jobRole);
     this._tempService.sendExperienceForm(this.experienceForm);
     // console.log(this.editBtnProperty);
-    if(this.editBtnProperty){
-      if(this.editBtnProperty === 'experience' || this.editBtnProperty === 'projects' || this.editBtnProperty === 'education'){
+    if (this.editBtnProperty) {
+      if (
+        this.editBtnProperty === 'experience' ||
+        this.editBtnProperty === 'projects' ||
+        this.editBtnProperty === 'education'
+      ) {
         this.isExpOrProjOrEdu = !this.isExpOrProjOrEdu;
       }
       return;
@@ -191,22 +266,97 @@ export class EditTemplateComponent implements OnInit, DoCheck {
     this.discardForm();
   }
 
+  ngAfterViewInit() {
+    this.skillButton1.forEach((element: ElementRef) => {
+      element.nativeElement.addEventListener('click', () => {
+        element.nativeElement.setAttribute('data-active', 'true');
+        this.skill1Value = element.nativeElement.value;
+        this._tempService.sendSkill1Value(this.skill1Value);
+        this.skillButton1.forEach((ele: ElementRef) => {
+          if (this.skill1Value != ele.nativeElement.value) {
+            ele.nativeElement.removeAttribute('data-active');
+          }
+        });
+      });
+    });
+    this.skillButton2.forEach((element: ElementRef) => {
+      element.nativeElement.addEventListener('click', () => {
+        element.nativeElement.setAttribute('data-active', 'true');
+        this.skill2Value = element.nativeElement.value;
+        this._tempService.sendSkill2Value(this.skill2Value);
+        this.skillButton2.forEach((ele: ElementRef) => {
+          if (this.skill2Value != ele.nativeElement.value) {
+            ele.nativeElement.removeAttribute('data-active');
+          }
+        });
+      });
+    });
+    this.skillButton3.forEach((element: ElementRef) => {
+      element.nativeElement.addEventListener('click', () => {
+        element.nativeElement.setAttribute('data-active', 'true');
+        this.skill3Value = element.nativeElement.value;
+        this._tempService.sendSkill3Value(this.skill3Value);
+        this.skillButton3.forEach((ele: ElementRef) => {
+          if (this.skill3Value != ele.nativeElement.value) {
+            ele.nativeElement.removeAttribute('data-active');
+          }
+        });
+      });
+    });
+    this.skillButton4.forEach((element: ElementRef) => {
+      element.nativeElement.addEventListener('click', () => {
+        element.nativeElement.setAttribute('data-active', 'true');
+        this.skill4Value = element.nativeElement.value;
+        this._tempService.sendSkill4Value(this.skill4Value);
+        this.skillButton4.forEach((ele: ElementRef) => {
+          if (this.skill4Value != ele.nativeElement.value) {
+            ele.nativeElement.removeAttribute('data-active');
+          }
+        });
+      });
+    });
+    this.skillButton5.forEach((element: ElementRef) => {
+      element.nativeElement.addEventListener('click', () => {
+        element.nativeElement.setAttribute('data-active', 'true');
+        this.skill5Value = element.nativeElement.value;
+        this._tempService.sendSkill5Value(this.skill5Value);
+        this.skillButton5.forEach((ele: ElementRef) => {
+          if (this.skill5Value != ele.nativeElement.value) {
+            ele.nativeElement.removeAttribute('data-active');
+          }
+        });
+      });
+    });
+    this.skillButton6.forEach((element: ElementRef) => {
+      element.nativeElement.addEventListener('click', () => {
+        element.nativeElement.setAttribute('data-active', 'true');
+        this.skill6Value = element.nativeElement.value;
+        this._tempService.sendSkill6Value(this.skill6Value);
+        this.skillButton6.forEach((ele: ElementRef) => {
+          if (this.skill6Value != ele.nativeElement.value) {
+            ele.nativeElement.removeAttribute('data-active');
+          }
+        });
+      });
+    });
+    this._changeDetector.detectChanges();
+  }
+
   resetForm() {
     this.resumeForm.reset();
   }
 
-  discardForm(){
+  discardForm() {
     this.editBtnProperty = '';
   }
-
 }
 
 export class CommonForm {
-    "title": '';
-    "organization": '';
-    "roleInProj": '';
-    "startingYear": '';
-    "endingYear": '';
-    "point1": '';
-    "point2": '';
+  'title': '';
+  'organization': '';
+  'roleInProj': '';
+  'startingYear': '';
+  'endingYear': '';
+  'point1': '';
+  'point2': '';
 }
--- src/app/templates/template-one/template-one.component.ts
+++ src/app/templates/template-one/template-one.component.ts
@@ -1,4 +1,17 @@
-import { Component, DoCheck, Input, OnInit } from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  QueryList,
+  SimpleChange,
+  SimpleChanges,
+  ViewChildren,
+} from '@angular/core';
 import { TemplateService } from 'src/app/template/template.service';
 import { CommonForm } from '../edit-template/edit-template.component';
 
@@ -7,19 +20,21 @@ import { CommonForm } from '../edit-template/edit-template.component';
   templateUrl: './template-one.component.html',
   styleUrls: ['./template-one.component.css'],
 })
-export class TemplateOneComponent implements OnInit, DoCheck {
+export class TemplateOneComponent
+  implements OnInit, DoCheck, AfterViewInit, OnChanges, AfterViewChecked
+{
   public firstName = '';
   public jobRole = '';
   public experienceForm: ExperienceForm = new CommonForm();
   public projectsForm: ExperienceForm = new CommonForm();
   public educationForm: ExperienceForm = new CommonForm();
-  public contactForm:any = {
+  public contactForm: any = {
     address: 'AP, India- 522124',
     email: '',
     phone: '9492117926',
     site: 'github.com/winay-chowdary66',
   };
-  public skillsForm:any = {
+  public skillsForm: any = {
     skill1: 'Python',
     skill2: 'Java',
     skill3: 'C++',
@@ -35,6 +50,21 @@ export class TemplateOneComponent implements OnInit, DoCheck {
     contact: 'contact',
     skills: 'skills',
   };
+
+  public skill1Value: string = '';
+  public skill2Value: string = '';
+  public skill3Value: string = '';
+  public skill4Value: string = '';
+  public skill5Value: string = '';
+  public skill6Value: string = '';
+
+  @ViewChildren('skillOpct1') skillOpct1!: QueryList<ElementRef>;
+  @ViewChildren('skillOpct2') skillOpct2!: QueryList<ElementRef>;
+  @ViewChildren('skillOpct3') skillOpct3!: QueryList<ElementRef>;
+  @ViewChildren('skillOpct4') skillOpct4!: QueryList<ElementRef>;
+  @ViewChildren('skillOpct5') skillOpct5!: QueryList<ElementRef>;
+  @ViewChildren('skillOpct6') skillOpct6!: QueryList<ElementRef>;
+
   constructor(private _tempService: TemplateService) {}
 
   ngOnInit(): void {
@@ -69,10 +99,84 @@ export class TemplateOneComponent implements OnInit, DoCheck {
       if (msg == null) return;
       this.skillsForm = msg;
     });
+    this._tempService.getSkill1Value().subscribe((msg) => {
+      if (msg == null) return;
+      this.skill1Value = msg;
+    });
+    this._tempService.getSkill2Value().subscribe((msg) => {
+      if (msg == null) return;
+      this.skill2Value = msg;
+    });
+    this._tempService.getSkill3Value().subscribe((msg) => {
+      if (msg == null) return;
+      this.skill3Value = msg;
+    });
+    this._tempService.getSkill4Value().subscribe((msg) => {
+      if (msg == null) return;
+      this.skill4Value = msg;
+    });
+    this._tempService.getSkill5Value().subscribe((msg) => {
+      if (msg == null) return;
+      this.skill5Value = msg;
+    });
+    this._tempService.getSkill6Value().subscribe((msg) => {
+      if (msg == null) return;
+      this.skill6Value = msg;
+    });
   }
 
-  ngDoCheck(): void {
-    // console.log(this.projectsForm);
+  ngDoCheck(): void {}
+
+  ngAfterViewInit(): void {
+  }
+  ngAfterViewChecked(): void {
+    this.skillOpct1.forEach((element, index) => {
+      if (this.skill1Value >= String(index + 1)) {
+        element.nativeElement.setAttribute('data-active', 'true');
+      }else{
+        element.nativeElement.removeAttribute('data-active');
+      }
+    });
+    this.skillOpct2.forEach((element, index) => {
+      if (this.skill2Value >= String(index + 1)) {
+        element.nativeElement.setAttribute('data-active', 'true');
+      } else {
+        element.nativeElement.removeAttribute('data-active');
+      }
+    });
+    this.skillOpct3.forEach((element, index) => {
+      if (this.skill3Value >= String(index + 1)) {
+        element.nativeElement.setAttribute('data-active', 'true');
+      }else {
+        element.nativeElement.removeAttribute('data-active');
+      }
+    });
+    this.skillOpct4.forEach((element, index) => {
+      if (this.skill4Value >= String(index + 1)) {
+        element.nativeElement.setAttribute('data-active', 'true');
+      }else {
+        element.nativeElement.removeAttribute('data-active');
+      }
+    });
+    this.skillOpct5.forEach((element, index) => {
+      if (this.skill5Value >= String(index + 1)) {
+        element.nativeElement.setAttribute('data-active', 'true');
+      }else {
+        element.nativeElement.removeAttribute('data-active');
+      }
+    });
+    this.skillOpct6.forEach((element, index) => {
+      if (this.skill6Value >= String(index + 1)) {
+        element.nativeElement.setAttribute('data-active', 'true');
+      }else {
+        element.nativeElement.removeAttribute('data-active');
+      }
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    console.log(this.skill2Value);
   }
 
   sendBtnProperty(property: string) {
